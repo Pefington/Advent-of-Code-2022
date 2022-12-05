56e6702dd943d8e6cef32a2da5dc4c70efc1d9f5
Pierre-François Salmand <maabaa@gmail.com>
2022-12-05 14:36:47 +0700

    Solve 5-1

--- 05/index.js
+++ 05/index.js
@@ -1,9 +1,33 @@
-import data from './input.json' assert { type: 'json' }
+import input from './input.json' assert { type: 'json' }
 
 //Part 1
+let stacks = input.slice(0, 3)
+let rest = input.slice(3)
 
+let instructions = rest.map((instruction) =>
+  instruction.split(' ').filter((word) => parseInt(word))
+)
 
-// const part1 = `  Part 1: ${overlapFully}`
+for (let instruction of instructions) {
+  for (
+    let cratesToMove = instruction[0],
+      stackFrom = stacks[instruction[1] - 1],
+      stackTo = stacks[instruction[2] - 1];
+    cratesToMove > 0;
+    cratesToMove--
+  ) {
+    stackTo.push(stackFrom.pop())
+  }
+}
+
+let result1 = []
+for ( let stack of stacks ) {
+  result1.push(stack.pop())
+}
+result1 = result1.join('')
+
+const part1 = `  Part 1: ${result1}`
+console.log(part1)
 // const part2 = `  Part 2: ${overlapAtAll}`
 
 // export default ['Day 04:', part1, part2]
