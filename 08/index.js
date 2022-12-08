9c66aff0ca1a45834c4a0344f853d813e7ebf792
Pierre-François Salmand <maabaa@gmail.com>
2022-12-08 12:48:41 +0700

    Start 8

---
+++ 08/index.js
@@ -0,0 +1,8 @@
+import input from './input.json' assert { type: 'json' }
+import util from 'util'
+
+const inspect = (obj) =>
+  console.log(util.inspect(obj, false, null, true /* enable colors */))
+const log = (...args) => console.log('\n', ...args, '\n\n', '-'.repeat(40))
+
+//Part 1
