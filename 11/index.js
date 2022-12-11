e5715aade0e2c02630718ae6988e15e66610c3cd
Pierre-François Salmand <maabaa@gmail.com>
2022-12-11 14:51:14 +0700

    Solve 11-1

--- 11/index.js
+++ 11/index.js
@@ -3,3 +3,40 @@ import input from './input.json' assert { type: 'json' }
 const log = (...args) => console.log('\n', ...args, '\n\n', '-'.repeat(40))
 
 //Part 1
+const monkeys = input
+let inspections = []
+
+for ( let monkey of monkeys ) {
+  monkey.inspected = 0
+}
+
+for ( let round = 1; round <= 20; round++ ) {
+
+  for ( let monkey of monkeys ) {
+
+    while (monkey.items.length > 0) {
+      let item = monkey.items[0]
+      monkey.inspected++
+      item = eval(monkey.operation)
+      item = Math.floor(item / 3)
+
+      if (item % monkey.test === 0) {
+        monkeys[monkey.true].items.push(item)
+        monkey.items.shift()
+      }
+
+      else {
+        monkeys[monkey.false].items.push(item)
+        monkey.items.shift()
+      }
+    }
+  }
+}
+
+for ( let monkey of monkeys ) {
+  inspections.push(monkey.inspected)
+}
+let monkeyBusiness = inspections.sort( ( a, b ) => b - a ).slice( 0, 2 )
+monkeyBusiness = monkeyBusiness[0] * monkeyBusiness[1]
+
+log('Part 1:', monkeyBusiness)
