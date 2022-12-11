fd6797903492179011da8330c6c661ecd86716e5
Pierre-François Salmand <maabaa@gmail.com>
2022-12-11 16:36:58 +0700

    Solve 11-2

--- 11/index.js
+++ 11/index.js
@@ -3,40 +3,87 @@ import input from './input.json' assert { type: 'json' }
 const log = (...args) => console.log('\n', ...args, '\n\n', '-'.repeat(40))
 
 //Part 1
-const monkeys = input
-let inspections = []
+const run1 = (rounds) => {
+  const monkeys = input
+  let inspections = []
 
-for ( let monkey of monkeys ) {
-  monkey.inspected = 0
-}
+  for (let monkey of monkeys) {
+    monkey.inspected = 0
+  }
 
-for ( let round = 1; round <= 20; round++ ) {
+  for (let round = 1; round <= rounds; round++) {
+    for (let monkey of monkeys) {
+      while (monkey.items.length > 0) {
+        let item = monkey.items[0]
+        monkey.inspected += 1
+        item = eval(monkey.operation)
+        item = Math.floor(item / 3)
 
-  for ( let monkey of monkeys ) {
-
-    while (monkey.items.length > 0) {
-      let item = monkey.items[0]
-      monkey.inspected++
-      item = eval(monkey.operation)
-      item = Math.floor(item / 3)
-
-      if (item % monkey.test === 0) {
-        monkeys[monkey.true].items.push(item)
-        monkey.items.shift()
-      }
-
-      else {
-        monkeys[monkey.false].items.push(item)
-        monkey.items.shift()
+        if (item % monkey.test === 0) {
+          monkeys[monkey.true].items.push(item)
+          monkey.items.shift()
+        } else {
+          monkeys[monkey.false].items.push(item)
+          monkey.items.shift()
+        }
       }
     }
   }
+
+  for (let monkey of monkeys) {
+    inspections.push(monkey.inspected)
+  }
+  let monkeyBusiness = inspections.sort((a, b) => b - a).slice(0, 2)
+  monkeyBusiness = monkeyBusiness[0] * monkeyBusiness[1]
+  log(monkeyBusiness)
+  return monkeyBusiness
 }
 
-for ( let monkey of monkeys ) {
-  inspections.push(monkey.inspected)
-}
-let monkeyBusiness = inspections.sort( ( a, b ) => b - a ).slice( 0, 2 )
-monkeyBusiness = monkeyBusiness[0] * monkeyBusiness[1]
+// run1(20)
 
-log('Part 1:', monkeyBusiness)
+//Part 2
+
+const run2 = (rounds) => {
+  const monkeys = input
+  let inspections = []
+  let divisors = []
+
+  for (let monkey of monkeys) {
+    monkey.inspected = 0
+    divisors.push(monkey.test)
+  }
+
+  const lcm = divisors.reduce((a, b) => a * b)
+  log(lcm)
+
+  for (let round = 1; round <= rounds; round++) {
+    for (let monkey of monkeys) {
+      while (monkey.items.length > 0) {
+        let item = monkey.items[0]
+        if ( item > lcm ) item = item % lcm
+        monkey.inspected += 1
+        item = eval(monkey.operation)
+
+        if (item % monkey.test === 0) {
+          monkeys[monkey.true].items.push(item)
+          monkey.items.shift()
+        } else {
+          monkeys[monkey.false].items.push(item)
+          monkey.items.shift()
+        }
+      }
+    }
+    if (round === 1 || round === 20 || round === 1000 || round % 1000 === 0)
+      log(round, monkeys)
+  }
+
+  for (let monkey of monkeys) {
+    inspections.push(monkey.inspected)
+  }
+  let monkeyBusiness = inspections.sort((a, b) => b - a).slice(0, 2)
+  monkeyBusiness = monkeyBusiness[0] * monkeyBusiness[1]
+  log(monkeyBusiness)
+  return monkeyBusiness
+}
+
+run2(10000)
