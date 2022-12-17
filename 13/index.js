5c6121f68d2ddd36f565192ebefc8fc79d049136
Pierre-François Salmand <maabaa@gmail.com>
2022-12-17 17:22:23 +0700

    WIP 13

--- 13/index.js
+++ 13/index.js
@@ -1,18 +1,48 @@
-import fs from 'fs'
-import util from 'util'
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import fs from 'fs';
+import util from 'util';
 
 const inspect = (obj) =>
-  console.log(util.inspect(obj, false, null, true /* enable colors */))
-const log = (...args) => console.log('\n', ...args, '\n\n', '-'.repeat(40))
+  console.log(util.inspect(obj, false, null, true /* enable colors */));
+const log = (...args) => console.log('\n', ...args, '\n\n', '-'.repeat(40));
 
-//Part 1
-const input = JSON.parse(fs.readFileSync('./input.txt', 'utf8').trimEnd())
-const left = [], right = []
+// Part 1
+const raw = fs.readFileSync('./input.txt', 'utf8').split('---');
+const example = raw[0].split('\n').filter(Boolean);
+const actual = raw[1].split('\n').filter(Boolean);
 
-for (let line of input) {
-  input.indexOf(line) % 2 === 0 ? left.push(line) : right.push(line)
+const input = example; // * Switch when ready
+
+const left = [];
+const right = [];
+
+input.forEach((line, index) => {
+  if (index % 2 === 0) {
+    left.push(JSON.parse(line));
+  } else {
+    right.push(JSON.parse(line));
+  }
+});
+
+function checkPair(lVal, rVal) {
+  return true;
 }
 
-inspect(left)
-log( '' )
-inspect(right)
+function run1(lInput, rInput) {
+  const validPairs = [];
+  for (let i = 0, max = lInput.length; i < max; i += 1) {
+    console.log(`\n== Pair ${i + 1} ==`);
+    console.log('- Compare', lInput[i], 'vs', rInput[i]);
+    if (checkPair(lInput[i], rInput[i])) {
+      console.log('GOTTEM!!!');
+      validPairs.push(i + 1);
+    }
+    log(validPairs);
+  }
+  const res = validPairs.reduce((sum, i) => sum + i);
+  log(res);
+  return res;
+}
+
+run1(left, right);
