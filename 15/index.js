3e2359f3b7ae4cae7c3b6019d6ab9bdab77cba23
Pierre-François Salmand <maabaa@gmail.com>
2023-01-03 09:16:53 +0700

    Refactor 15-1

--- 15/index.js
+++ 15/index.js
@@ -1,15 +1,17 @@
 import fs from 'fs';
-import util from 'util';
 
-// Part 1
-let raw = fs
+// * Part 1
+const raw = fs
   .readFileSync('./input.txt', 'utf8')
   .split('---')
   .map((line) => line.split('\n').filter(Boolean));
 
+// eslint-disable-next-line no-unused-vars
 const [example, actual] = raw;
 
-let input = actual.map((line) =>
+// const input = example.map((line) =>
+  // !Swap above and below lines when ready
+  const input = actual.map((line) =>
   line
     .split(' ')
     .filter((arr) => arr.includes('='))
@@ -21,24 +23,46 @@ let input = actual.map((line) =>
     )
 );
 
-function count(data, targetY) {
-  let targetLine = new Set()
-  data.forEach((line) => {
-    const [x, y, xB, yB] = [...line.map((str) => parseInt(str, 10))];
-    const distance = Math.abs(yB - y) + Math.abs(xB - x);
+const squares = []
+const sensors = new Set()
+const beacons = new Set()
+
+input.forEach((line) => {
+  const [xS, yS, xB, yB] = [...line.map( ( str ) => parseInt( str, 10 ) )];
+  const sensor = [xS, yS]
+  const beacon = [xB, yB]
+  const distance = Math.abs(yB - yS) + Math.abs(xB - xS)
+  squares.push([sensor, beacon, distance])
+  sensors.add(`${xS} ${yS}`)
+  beacons.add(`${xB} ${yB}`)
+});
+
+function countPositions(areas, targetY) {
+  const targetLine = new Set();
+  areas.forEach((square) => {
+    const [xS, yS] = square[0]
+    const [xB, yB] = square[1]
+    const distance = square[2]
 
     if (
-      (y <= targetY && y + distance >= targetY) ||
-      (y >= targetY && y - distance <= targetY)
+      (yS <= targetY && yS + distance >= targetY) ||
+      (yS >= targetY && yS - distance <= targetY)
     ) {
-      const xSpread = Math.abs( distance - Math.abs( targetY - y ) );
-      const [minSpread, maxSpread] = [x - xSpread, x + xSpread]
-      for ( let val = minSpread; val <= maxSpread; val += 1 ) {
-        if (!(yB === targetY && val === xB)) targetLine.add( val )
+      const xSpread = Math.abs(distance - Math.abs(targetY - yS));
+      const [minSpread, maxSpread] = [xS - xSpread, xS + xSpread];
+      for (let val = minSpread; val <= maxSpread; val += 1) {
+        if (!(yB === targetY && val === xB)) targetLine.add(val);
       }
     }
   });
-  console.log(targetLine.size)
+  // eslint-disable-next-line no-console
+  console.log( targetLine.size, '\n' );
 }
 
-count(input, 2000000);
+countPositions( squares, 2000000 );
+
+console.log(squares)
+
+// * Part 2
+function getFrequency(data, limit) {}
+// getFrequency(input, 4000000);
